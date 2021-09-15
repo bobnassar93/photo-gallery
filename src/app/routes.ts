9ffe96067126c5da3009bb68def1d3b09e97c798
Bob Nassar <nassarbob@outlook.com>
2021-09-15 15:40:08 +0300

    Minor change

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,7 +3,7 @@ import { GalleryComponent } from "../app/gallery/gallery.component"
 import { ImageDetailComponent } from "../app/image-detail/image-detail.component"
 
 export const appRoutes: Routes = [
-    { path: "gallery", component: GalleryComponent },
-    { path: "image/:id", component: ImageDetailComponent },
-    { path: "", redirectTo: "/gallery", pathMatch: 'full' },
+  { path: "gallery", component: GalleryComponent },
+  { path: "image/:id", component: ImageDetailComponent },
+  { path: "", redirectTo: "gallery", pathMatch: 'full' },
 ]
